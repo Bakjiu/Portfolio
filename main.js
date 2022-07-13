526028236da897303cb4c1e8dc95f3cf65f53f80
Bakjiu <jxxwxxx@gmail.com>
2022-07-13 15:24:59 +0900

    JS: Navbar menu click

--- main.js
+++ main.js
@@ -13,4 +13,19 @@ document.addEventListener("scroll", () => {
   } else {
     navbar.classList.remove("navbar--dark");
   }
+
+  // Handle scrollint when tapping on the navbar menu
+  const navbarMenu = document.querySelector(".navbar__menu");
+
+  navbarMenu.addEventListener("click", (e) => {
+    const target = e.target;
+    const link = target.dataset.link;
+    if (link == null) {
+      return;
+    } else {
+      console.log(link);
+      const scrollTo = document.querySelector(link);
+      scrollTo.scrollIntoView({ behavior: "smooth" });
+    }
+  });
 });
